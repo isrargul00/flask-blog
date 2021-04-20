1b109bcdb1cd3cb86677b0ea73e1173ca38b0182
israr <isrargul00@gmail.com>
2021-04-21 02:54:24 +0500

    processing

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+db = SQLAlchemy(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('layout.html')
 
 if __name__ == '__main__':
     app.run()
